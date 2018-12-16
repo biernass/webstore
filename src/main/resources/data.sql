05e23709263cb947f7d1ecc993037a12f9d32f83
Michał Biernacki <micbiernacki@gmail.com>
2018-12-16 22:05:36 +0100

    first item from database added and ProductController implementation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(product_id, category, description, manufacturer, name,unit_price, units_in_stock, discounted)
+ VALUES (1, 'ELECTRONICS', 'Opis pierwszego przedmiotu Opis pierwszego przedmiotu','APPLE', 'iPhone 6S',1000, 200, false );
+
+
