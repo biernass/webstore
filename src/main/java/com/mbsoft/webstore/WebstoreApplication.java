b914b7ac7c36bc98a9fd805e3deca6e83b5d03e3
Michał Biernacki <micbiernacki@gmail.com>
2019-01-04 19:06:27 +0100

    getProductById

--- src/main/java/com/mbsoft/webstore/WebstoreApplication.java
+++ src/main/java/com/mbsoft/webstore/WebstoreApplication.java
@@ -3,6 +3,7 @@ package com.mbsoft.webstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
--- src/main/java/com/mbsoft/webstore/controllers/ProductController.java
+++ src/main/java/com/mbsoft/webstore/controllers/ProductController.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 import java.util.Map;
@@ -42,6 +40,12 @@ public class ProductController {
     public String getProductsByFilter(@MatrixVariable(pathVar = "ByCriteria") Map<String, List<String>> filterParams, Model model) {
         model.addAttribute("products", productService.getProductsByFilter(filterParams));
         return "products";
+    }
+
+    @GetMapping("/product")
+    public String getProductbyId(@RequestParam("id") Long productId, Model model) {
+        model.addAttribute("product", productService.getProductById(productId));
+        return "product";
     }
 }
 
